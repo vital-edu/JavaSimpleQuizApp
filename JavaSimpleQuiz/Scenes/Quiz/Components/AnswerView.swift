e70b26dc7f11bff269075f03b06d5fed833318d9
Eduardo Vital <vitaldu@gmail.com>
2019-08-18 16:56:59 -0300

    Add table view to AnswerView

--- JavaSimpleQuiz/Scenes/Quiz/Components/AnswerView.swift
+++ JavaSimpleQuiz/Scenes/Quiz/Components/AnswerView.swift
@@ -9,17 +9,48 @@
 import Foundation
 import UIKit
 
-class AnswerView : UIStackView {
+class AnswerView : UIView, UITableViewDelegate, UITableViewDataSource {
+    private let cellRegisterId = "AnswerViewCell"
+
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellRegisterId)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.separatorInset = .zero
+
+        return tableView
+    }()
+
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupView()
     }
 
-    required init(coder aDecoder: NSCoder) {
+    required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         setupView()
     }
 
     private func setupView() {
+        self.addSubview(tableView)
+
+        tableView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        tableView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+        tableView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellRegisterId)!
+        cell.textLabel?.text = "test"
+
+        return cell
     }
 }
